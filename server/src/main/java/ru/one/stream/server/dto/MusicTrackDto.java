710c4742fc2d75250ecc0d32d8306c1d72fa3ec8
Sergei Kusakin <kusakinsv@mail.ru>
2026-07-08 08:19:36 +0500

    игнорирование типов

--- server/src/main/java/ru/one/stream/server/dto/MusicTrackDto.java
+++ server/src/main/java/ru/one/stream/server/dto/MusicTrackDto.java
@@ -1,10 +1,12 @@
 package ru.one.stream.server.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MusicTrackDto {
 
     private Long id;
