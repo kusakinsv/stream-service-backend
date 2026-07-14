fbad1db73d59269ea5f96bb0b12ae9d5514484d2
Sergei Kusakin <kusakinsv@mail.ru>
2026-07-14 17:57:02 +0500

    полная трансформация

--- internet-searcher/src/main/java/ru/one/stream/internetsercher/service/freemusicstores/Muzmo.java
+++ internet-searcher/src/main/java/ru/one/stream/internetsercher/service/freemusicstores/Muzmo.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Component;
 import ru.one.stream.internetsercher.models.SearchedMusicTrack;
 import ru.one.stream.internetsercher.utils.Utils;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,7 +21,8 @@ import java.util.Set;
  */
 @Component
 public class Muzmo implements MusicResource {
-    public static final String URL = "https://su.muzmo.cc";
+//    public static final String URL = "https://su.muzmo.cc";
+    public static final String URL = "https://rmr.muzmo.cc";
     public static final String IP = "http://5.79.82.86";//не работает
     public static final String QUERY_URL = URL + "/search?q=";
     public static final String QUERY_IP = IP + "/search?q=";
@@ -26,11 +30,14 @@ public class Muzmo implements MusicResource {
     @SneakyThrows
     @Override
     public Set<SearchedMusicTrack> search(String trackName) {
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
         String query = QUERY_URL + Utils.toConvertedStringWithPlus(trackName);
         Document document = Jsoup.connect(query)
                 .followRedirects(true)
                 .maxBodySize(0)
                 .timeout(10000)
+                .sslContext(sslContext)
                 .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36")
                 .referrer("http://www.google.com")
                 .get();
@@ -47,4 +54,16 @@ public class Muzmo implements MusicResource {
 
         return tracks;
     }
+
+    TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                }
+            }
+    };
 }
